85f1dff81e99391472060ca00bc2cdfb38654676
Victorique Liutenko <victorique@liutenko.me>
2018-06-22 13:38:32 +0300

    Fix some issues

--- components/helpers.js
+++ components/helpers.js
@@ -1,21 +1,21 @@
 function addClass(element, classStr) {
-	let classesArray = element.className.split(" ");
+  let classesArray = element.className.split(" ");
 
-	if(classesArray.indexOf(classStr) === -1) {
-		classesArray.push(classStr);
-	}
+  if(classesArray.indexOf(classStr) === -1) {
+    classesArray.push(classStr);
+  }
 
-	let newClassesStr = classesArray.join(" ");
-	element.className = newClassesStr;
+  let newClassesStr = classesArray.join(" ");
+  element.className = newClassesStr;
 }
 
 function removeClass(element, classStr) {
-	let classesArray = element.className.split(" ");
+  let classesArray = element.className.split(" ");
 
-	let newClassesArray = classesArray.filter(function(current) {
-		return current !== classStr;
-	});
+  let newClassesArray = classesArray.filter(function(current) {
+    return current !== classStr;
+  });
 
-	let newClassesStr = newClassesArray.join(" ");
-	element.className = newClassesStr;
+  let newClassesStr = newClassesArray.join(" ");
+  element.className = newClassesStr;
 }
--- components/ourServicesTile/ourServicesTile.js
+++ components/ourServicesTile/ourServicesTile.js
@@ -1,43 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-	let knowMoreArray = document.getElementsByClassName("js-knowMoreButton");
+  let knowMoreArray = document.getElementsByClassName("js-knowMoreButton");
 
-	for (let i = 0; i < knowMoreArray.length; i++) {
-	  knowMoreArray[i].onclick = function knowMore() {
-			let infoBlock = this.closest(".js-infoBlock");
-			let bg = infoBlock.querySelector(".ourServicesTile-transparentColorBlock--gray");
-			let infoWrap = infoBlock.querySelector(".ourServicesTile-infoWrapper");
-			let cross = infoBlock.querySelector(".ourServicesTile-buttonImage--cross");
-			let smallText = infoBlock.querySelector(".js-smallText");
-			let fullText = infoBlock.querySelector(".js-fullText");
+  for (let i = 0; i < knowMoreArray.length; i++) {
+    knowMoreArray[i].onclick = function knowMore() {
+      let infoBlock = this.closest(".js-infoBlock");
+      let bg = infoBlock.querySelector(".ourServicesTile-transparentColorBlock--gray");
+      let infoWrap = infoBlock.querySelector(".ourServicesTile-infoWrapper");
+      let cross = infoBlock.querySelector(".ourServicesTile-buttonImage--cross");
+      let smallText = infoBlock.querySelector(".js-smallText");
+      let fullText = infoBlock.querySelector(".js-fullText");
 
-			addClass(this, "helpers-displayNone");
-			addClass(infoWrap, "ourServicesTile-infoWrapper--cyan");
-			removeClass(cross, "helpers-displayNone");
-			removeClass(bg, "ourServicesTile-transparentColorBlock--gray");
-			addClass(bg, "ourServicesTile-transparentColorBlock--cyan");
-			addClass(smallText, "helpers-displayNone");
-			removeClass(fullText, "helpers-displayNone");
-		}
-	}
+      addClass(this, "helpers-displayNone");
+      addClass(infoWrap, "ourServicesTile-infoWrapper--cyan");
+      removeClass(cross, "helpers-displayNone");
+      removeClass(bg, "ourServicesTile-transparentColorBlock--gray");
+      addClass(bg, "ourServicesTile-transparentColorBlock--cyan");
+      addClass(smallText, "helpers-displayNone");
+      removeClass(fullText, "helpers-displayNone");
+    }
+  }
 
-	let crossArray = document.getElementsByClassName("js-crossButton");
+  let crossArray = document.getElementsByClassName("js-crossButton");
 
-	for (let i = 0; i < crossArray.length; i++) {
-	  crossArray[i].onclick = function() {
-			let infoBlock = this.closest(".js-infoBlock");
-			let bg = infoBlock.querySelector(".ourServicesTile-transparentColorBlock--cyan");
-			let infoWrap = infoBlock.querySelector(".ourServicesTile-infoWrapper");
-			let knowMore = infoBlock.querySelector(".ourServicesTile-buttonImage--know-more");
-			let smallText = infoBlock.querySelector(".js-smallText");
-			let fullText = infoBlock.querySelector(".js-fullText");
+  for (let i = 0; i < crossArray.length; i++) {
+    crossArray[i].onclick = function() {
+      let infoBlock = this.closest(".js-infoBlock");
+      let bg = infoBlock.querySelector(".ourServicesTile-transparentColorBlock--cyan");
+      let infoWrap = infoBlock.querySelector(".ourServicesTile-infoWrapper");
+      let knowMore = infoBlock.querySelector(".ourServicesTile-buttonImage--know-more");
+      let smallText = infoBlock.querySelector(".js-smallText");
+      let fullText = infoBlock.querySelector(".js-fullText");
 
-			addClass(this, "helpers-displayNone");
-			removeClass(infoWrap, "ourServicesTile-infoWrapper--cyan");
-			removeClass(knowMore, "helpers-displayNone");
-			addClass(bg, "ourServicesTile-transparentColorBlock--gray");
-			removeClass(bg, "ourServicesTile-transparentColorBlock--cyan");
-			removeClass(smallText, "helpers-displayNone");
-			addClass(fullText, "helpers-displayNone");
-		}
-	}
+      addClass(this, "helpers-displayNone");
+      removeClass(infoWrap, "ourServicesTile-infoWrapper--cyan");
+      removeClass(knowMore, "helpers-displayNone");
+      addClass(bg, "ourServicesTile-transparentColorBlock--gray");
+      removeClass(bg, "ourServicesTile-transparentColorBlock--cyan");
+      removeClass(smallText, "helpers-displayNone");
+      addClass(fullText, "helpers-displayNone");
+    }
+  }
 })
